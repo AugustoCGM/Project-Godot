3af8e6aacd23d8fea1e945972aed74314199ce06
AugustoCGM <augusto.cgm020306@gmail.com>
2026-09-07 19:05:10 -0300

    fisica de queda

--- scripts/player.gd
+++ scripts/player.gd
@@ -12,12 +12,12 @@ func _physics_process(delta: float) -> void:
 		velocity += get_gravity() * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction := Input.get_axis("ui_left", "ui_right")
+	var direction := Input.get_axis("left", "right")
 	if direction:
 		velocity.x = direction * SPEED
 	else:
@@ -34,12 +34,16 @@ func _physics_process(delta: float) -> void:
 			ani.play("idle")
 
 	else:
-		ani.play("jump")
+		# Verifica se a velocidade no eixo Y é maior que 0 (caindo)
+		if velocity.y > 0:
+			ani.play("falling")
+		else:
+			ani.play("jump")
+		
+		#virar o sprite enquanto o personagem está no ar
 		if direction > 0:
 			ani.flip_h = false
 		elif direction < 0:
 			ani.flip_h = true
-		
-		
 
 	move_and_slide()
